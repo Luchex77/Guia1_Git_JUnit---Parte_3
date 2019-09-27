4085888bc30d5221c79415a83b93355a12c2b6c9
Luchex77 <SalvadorLeonVilches2016@gmail.com>
2019-09-27 02:38:08 -0300

    El Principal con las 4 Op. Basicas

--- OperacionesBasicas/src/Principal.java
+++ OperacionesBasicas/src/Principal.java
@@ -11,6 +11,18 @@ public class Principal {
         double b = s.nextDouble();
         
         OperacionesBasicas op = new OperacionesBasicas();
+        System.out.println("La suma de ambos numeros ingresados es : ");
         System.out.println(op.Suma(a, b));
+        
+        System.out.println("La resta de ambos numeros ingresados es : ");
+        System.out.println(op.Resta(a, b));
+        
+         System.out.println("La multiplicacion de ambos numeros ingresados es : ");
+        System.out.println(op.Multiplicacion(a, b));
+        
+        System.out.println("La division de ambos numeros ingresados es : ");
+        System.out.println(op.Division(a, b));
+        
+        
     }
 }
